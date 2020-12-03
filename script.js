5ce5a2940b4ac81a246c21e52ad0cd2a8768df78
nutthss <66852393+nutthss@users.noreply.github.com>
2020-12-03 16:31:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,199 +1,36 @@
 $(document).ready(() => {
-  function embaralha(lista) {
-    let indice = lista.length;
+  $("#form").submit((event) => {
+    event.preventDefault();
 
-    while (indice) {
-      // atenção para o pós-incremento indice--
-      const indiceAleatorio = Math.floor(Math.random() * indice--);
-      [lista[indice], lista[indiceAleatorio]] = [
-        lista[indiceAleatorio],
-        lista[indice],
-      ];
+    const twitchName = $("#twitch_name").val();
+    const instagramName = $("#instagram_name").val();
+
+    if (
+      !twitchName ||
+      !instagramName
+    ) {
+      Swal.fire({
+        title: "Preencha todos os dados",
+        text: "Para participar você precisa seguir todo o regulamento",
+        icon: "error",
+        confirmButtonText: "Vou preencher",
+      });
+      return;
     }
-  }
 
-  const participantes = [
-    {
-      "instagramName" : "01._ferret",
-      "isSub" : false,
-      "isValid" : true,
-      "isVip" : false,
-      "twitchName" : "frt301"
-    },
-    {
-      "instagramName" : "@JackMitsuo",
-      "isSub" : false,
-      "isValid" : true,
-      "isVip" : false,
-      "twitchName" : "jack_mitsuo23"
-    },
-      {
-      "instagramName" : "StynieOfficial",
-      "isSub" : false,
-      "isValid" : true,
-      "isVip" : true,
-      "twitchName" : "Stynie"
-    },
-    {
-      "instagramName" : "marcos_aa3",
-      "isSub" : false,
-      "isValid" : true,
-      "isVip" : false,
-      "twitchName" : "VedderSM"
-    },
-      {
-      "instagramName" : "Doug_crf",
-      "isSub" : false,
-      "isValid" : true,
-      "isVip" : false,
-      "twitchName" : "masterdodo_"
-    },
-      {
-      "instagramName" : "cabeludosccp1611",
-      "isSub" : false,
-      "isValid" : true,
-      "isVip" : false,
-      "twitchName" : "mikael1611"
-    },
-      {
-      "instagramName" : "@thalysonteixeira",
-      "isSub" : false,
-      "isValid" : true,
-      "isVip" : false,
-      "twitchName" : "thalysonpc"
-    },
-      {
-      "instagramName" : "iGabireu",
-      "isSub" : false,
-      "isValid" : true,
-      "isVip" : false,
-      "twitchName" : "VejoCoisas"
-    },
-      {
-      "instagramName" : "misterioxd_",
-      "isSub" : false,
-      "isValid" : true,
-      "isVip" : false,
-      "twitchName" : "MisterioLives"
-    },
-      {
-      "instagramName" : "Deryck.Oliveira.3",
-      "isSub" : true,
-      "isValid" : true,
-      "isVip" : false,
-      "twitchName" : "deryckgostoso"
-    },
-      {
-      "instagramName" : "Não tenho",
-      "isSub" : false,
-      "isValid" : true,
-      "isVip" : false,
-      "twitchName" : "flavitoadr"
-    },
-      {
-      "instagramName" : "@andreferlaofc",
-      "isSub" : false,
-      "isValid" : true,
-      "isVip" : false,
-      "twitchName" : "P0largd"
-    },
-      {
-      "instagramName" : "p_gom3s",
-      "isSub" : true,
-      "isValid" : true,
-      "isVip" : false,
-      "twitchName" : "umcaradepauqu4lquer"
-    },
-      {
-      "instagramName" : "sonicfps10",
-      "isSub" : false,
-      "isValid" : true,
-      "isVip" : false,
-      "twitchName" : "sonicfps10"
-    },
-    {
-      "instagramName" : "gabrielprestes14",
-      "isSub" : false,
-      "isValid" : true,
-      "isVip" : false,
-      "twitchName" : "gabriel_pc14"
-    },
-    {
-    "instagramName" : "Kaue._vitor",
-    "isSub" : false,
-    "isValid" : true,
-    "isVip" : false,
-    "twitchName" : "Kauevitor66XT"
-  },
-  {
-    "instagramName" : "GMBM",
-    "isSub" : false,
-    "isValid" : true,
-    "isVip" : false,
-    "twitchName" : "Pengis"
-  },
-  {
-    "instagramName" : "Nicolas Eckhardt",
-    "isSub" : false,
-    "isValid" : true,
-    "isVip" : false,
-    "twitchName" : "MundonEck"
-  },
-  {
-    "instagramName" : "luisfepo",
-    "isSub" : true,
-    "isValid" : true,
-    "isVip" : false,
-    "twitchName" : "SraLuxanna"
-  },
-  {
-    "instagramName" : "cyrouxxx",
-    "isSub" : false,
-    "isValid" : true,
-    "isVip" : false,
-    "twitchName" : "cyrouxxx"
-  },
-  {
-    "instagramName" : "djierghiovhdi",
-    "isSub" : false,
-    "isValid" : true,
-    "isVip" : false,
-    "twitchName" : "djierghiovhdi"
-  }
-  ];
-  console.log(participantes);
-
-  const all = [];
-
-  participantes.forEach((participante) => {
-    if (participante.isValid) {
-      all.push(participante);
-      if (participante.isSub) {
-        all.push(participante);
-        all.push(participante);
-      }
-
-      if (participante.isVip) {
-        all.push(participante);
-      }
-    }
-  });
-
-  console.log(all);
-
-  // embaralha(all);
-
-  all.forEach((participante) => {
-    $("#lista").append(`
-    <li class="list-group-item" data-p="${participante.twitchName}">${participante.twitchName}</li>`);
-  });
-
-  $("#sortear").click(() => {
+    const data = {
+      twitchName,
+      instagramName,
+      isValid: false,
+      isSub: false,
+      isVip: false,
+    };
+    firebase.database().ref().child("participants").push(data);
     Swal.fire({
-      title: "O sorteado foi:",
-      text: all[Math.floor(Math.random() * all.length + 1)].twitchName,
+      title: "Sucesso",
+      text: "Seu cadastro foi realizado com sucesso. Boa sorte!",
       icon: "success",
-      confirmButtonText: "Ok",
+      confirmButtonText: "Vou preencher",
     });
   });
 });
